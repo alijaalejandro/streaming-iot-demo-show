6c4fac0c3f8c5e3fe30469f85ac8f1e682528548
Alejandro <alija.alejandro@gmail.com>
2024-10-12 20:00:34 +0200

    Update sender_dht22.py
    Co-Authored-By: Adrián García Allende <agallende@gmail.com>
    Co-Authored-By: agarciaallende <148314840+agarciaallende@users.noreply.github.com>

--- Demos/Demo_1_MQTT_and_IoT_Setup/code/sender_dht22.py
+++ Demos/Demo_1_MQTT_and_IoT_Setup/code/sender_dht22.py
@@ -47,7 +47,7 @@ def sensor_inside():
     #temperature = temperature + ct
     #temperature = temperature
     #humidity = humidity + ch
-    dew_point_inside = calculate_dew_point(temperature, humidity)
+    dew_point = calculate_dew_point(temperature, humidity)
 
     if humidity is not None and temperature is not None:
         print('Temp={0:0.1f}*C  Humidity={1:0.1f}%  Dewpoint={0:0.1f}*C'.format(temperature, humidity, dew_point))
