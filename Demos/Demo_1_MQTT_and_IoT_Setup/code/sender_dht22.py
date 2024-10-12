9f35854b35cedc4381af3fe09e382c4c42efff9f
Alejandro <alija.alejandro@gmail.com>
2024-10-12 19:10:21 +0200

    Update sender_dht22.py
    Co-Authored-By: Adrián García Allende <agallende@gmail.com>
    Co-Authored-By: agarciaallende <148314840+agarciaallende@users.noreply.github.com>

--- Demos/Demo_1_MQTT_and_IoT_Setup/code/sender_dht22.py
+++ Demos/Demo_1_MQTT_and_IoT_Setup/code/sender_dht22.py
@@ -43,7 +43,8 @@ def sensor_inside():
     ch = 10 
 
     temperature_inside_F, humidity_inside = Adafruit_DHT.read_retry(sensor, pin)
-    temperature_inside = (((temperature_inside_F - 32) / 1.8 ) / 10)
+    #temperature_inside = (((temperature_inside_F - 32) / 1.8 ) / 10)
+    temperature_inside = temperature_inside_F 
     temperature_inside = temperature_inside + ct
     humidity_inside = humidity_inside/ch
     dew_point_inside = calculate_dew_point(temperature_inside, humidity_inside)
